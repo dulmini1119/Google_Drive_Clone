01c38383b6c61d249063a32acf93f3bffa230c1a
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-11-10 21:39:39 +0530

    Create appwrite

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,21 +16,29 @@ import {
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
 
 type FormType = "sign-in" | "sign-up";
+
+const authFormSchema = (formType : FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName: formType === 'sign-up' ? z.string().min(2).max(50) : z.string().optional(),
+  });
+};
 
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading,setIsLoading] = useState(false);
   const [errorMessage,setErrorMessage] = useState('');
 
+  const formSchema = authFormSchema(type);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      fullName: "",
+      email: "",
     },
   });
 
@@ -48,7 +56,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
           {type === "sign-up" && (
             <FormField
               control={form.control}
-              name="fullname"
+              name="fullName"
               render={({ field }) => (
                 <FormItem>
                   <div className="shad-form-item">
@@ -98,7 +106,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
               alt="loader"
               width={24}
               height={24}
-              className="animate-spint ml-2"
+              className="animate-spin ml-2"
               />
             )}
           </Button>
@@ -108,12 +116,18 @@ const AuthForm = ({ type }: { type: FormType }) => {
           }
 
           <div className="body-2 flex justify-center">
-            <p>
+            <p className="text-light-100">
               {type === 'sign-in'
               ? "Don't have an account?"
               : "Already have an account?"
               }
             </p>
+
+              <Link href={type === 'sign-in' ? '/sign-up' : 'sign-in'}
+              className="ml-1 font-medium text-brand">
+                {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+              </Link>
+
           </div>
 
 
