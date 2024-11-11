81404574fc3ac772195ccaffd28ab8d7db4c7963
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-11-11 23:58:44 +0530

    Creating Layout,Nav,Sidebar

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
+import Search from '@/components/Search'
+import FileUploader from '@/components/FileUploader'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <Search/> 
+      <div className='header-wrapper'>
+        <FileUploader/> 
+        <form>
+          <Button className='sign-out-button' type='submit'>
+            <Image
+            src='/assets/icons/logout.svg'
+            alt='logout'
+            width={24}
+            height={24}
+            className='w-6'
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  )
+}
+
+export default Header
