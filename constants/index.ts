7f86a563e23b982eabff70da9cb07eb39864be7c
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-11-12 01:05:00 +0530

    Creating Sidebar

--- constants/index.ts
+++ constants/index.ts
@@ -25,3 +25,5 @@ export const navItems = [
         url : '/others',
     }
 ]
+
+export const avatarPlaceholderUrl = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 'use server'
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 const getUserByEmail = async (email: string) => {
     const {databases} = await createAdminClient();
@@ -55,7 +56,7 @@ export const createAccount = async ({
                 {
                     fullName,
                     email,
-                    avatar:'https://www.google.com/imgres?q=avatar%20icon&imgurl=https%3A%2F%2Fcdn.icon-icons.com%2Ficons2%2F2630%2FPNG%2F512%2Favatar_people_woman_girl_asian_icon_159134.png&imgrefurl=https%3A%2F%2Ficon-icons.com%2Ficon%2Favatar-people-woman-girl-asian%2F159134&docid=xbBlqkCjAiLjmM&tbnid=6HruKkAXgaXG4M&vet=12ahUKEwiTz9bD5dKJAxVuxDgGHZKOCrcQM3oECFAQAA..i&w=512&h=512&hcb=2&ved=2ahUKEwiTz9bD5dKJAxVuxDgGHZKOCrcQM3oECFAQAA',
+                    avatar:{avatarPlaceholderUrl},
                     accountId,
                 },
             );
@@ -80,4 +81,20 @@ export const createAccount = async ({
         } catch (error) {
             handleError(error,'Failed to verify OTP');
         }
+    }
+
+    export const getCurrentUser = async () => {
+        const {databases, account} = await createSessionClient();
+
+        const result = await account.get();
+
+        const user = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.usersCollectionId,
+            [Query.equal('accountId', result.$id)],
+        );
+
+        if (user.total < 0) return null;
+
+        return parseStringify(user.documents[0]);
     }
